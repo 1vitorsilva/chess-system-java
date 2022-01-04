40b971596a6b682ac9e2c1cb415274ba915ec9a2
Vitor Silva <vluis.silva1@gmail.com>
2022-01-04 18:50:26 -0300

    Starting to implement Board e Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
@@ -7,8 +8,7 @@ public class Program {
 	public static void main(String[] args) {
 	
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		Board board = new Board(8,8);
 
 	}
 
